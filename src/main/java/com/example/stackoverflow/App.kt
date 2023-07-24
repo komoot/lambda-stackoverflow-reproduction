8f511aa08b05643c3044f9682107a263172ac452
Martin Thurau <martin.thurau@komoot.de>
2023-07-24 12:33:01 +0200

    Reproducing example

---
+++ src/main/java/com/example/stackoverflow/App.kt
@@ -0,0 +1,79 @@
+package com.example.stackoverflow
+
+import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.RequestHandler
+import io.ktor.client.*
+import io.ktor.client.engine.apache.*
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.logging.*
+import io.ktor.client.request.*
+import kotlinx.coroutines.runBlocking
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.ServerSocket
+
+
+class App : RequestHandler<Any, Any> {
+    override fun handleRequest(input: Any, context: Context): Unit {
+        reproduce()
+    }
+}
+
+fun main() {
+    reproduce()
+}
+
+fun reproduce() {
+    val client = HttpClient(Apache) {
+        expectSuccess = false
+        engine {
+            // this is just so we can debug faster
+            socketTimeout = 500
+        }
+        install(Logging) {
+            logger = Logger.DEFAULT
+        }
+    }
+
+    val server = socketTimeoutThread()
+    server.start()
+
+    runBlocking {
+        println("Executing request")
+        client.get("http://localhost:$mockServerPort/")
+    }
+}
+
+private const val mockServerPort = 15467
+
+
+/**
+ * A one-thread-one-request socket server the reliable produces a socket timeout
+ */
+fun socketTimeoutThread() = Thread {
+    val serverSocket = ServerSocket(mockServerPort)
+    val clientSocket = serverSocket.accept()
+
+    val inputStream = BufferedReader(InputStreamReader(clientSocket.getInputStream()))
+    val outputStream = clientSocket.getOutputStream()
+
+    var line: String?
+
+    while (inputStream.readLine().also { line = it } != null) {
+        if (line!!.isEmpty()) {
+            break
+        }
+    }
+
+    // write partial response and then hang
+    outputStream.write("""HTTP/1.1 200 OK
+            Content-Type: application/json
+            
+            {""".trimIndent().toByteArray())
+    outputStream.flush()
+    try {
+        Thread.sleep(2000)
+    } catch (e: InterruptedException) {
+        // exit
+    }
+}
